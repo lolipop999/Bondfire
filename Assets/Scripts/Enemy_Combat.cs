4cfa332337abb5eebc696692592b51e98e9e0ddb
serena-tu <serena.tu90@gmail.com>
2025-06-08 10:15:26 -0400

    working character with an enemy attacker

---
+++ Assets/Scripts/Enemy_Combat.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Enemy_Combat : MonoBehaviour
+{
+    public int damage = 1;
+    public Transform attackPoint;
+    public float weaponRange;
+    public LayerMask playerLayer;
+
+    public void Attack()
+    {
+        Collider2D[] hits = Physics2D.OverlapCircleAll(attackPoint.position, weaponRange, playerLayer);
+        if (hits.Length > 0)
+        {
+            hits[0].GetComponent<PlayerHealth>().ChangeHealth(-damage);
+        }
+    }
+}
